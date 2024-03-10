9f899193e84d646221b03f68b0d4c65bd023f68f
it21183454 <99382245+shanulkshan@users.noreply.github.com>
2024-03-10 23:41:23 +0530

    change background color and remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Column(
           children: [
             Container(
